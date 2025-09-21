ccd59107898480d5b7a060155746a6592520efc1
Evgeniia Kharitonova <kuznetsovaevgeni@gmail.com>
2025-09-21 16:01:02 +0300

    changed owner for test

--- src/test/java/io/qameta/allure/IssuesRestTest.java
+++ src/test/java/io/qameta/allure/IssuesRestTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @Layer("rest")
-@Owner("baev")
+@Owner("evgenikuz")
 @Feature("Issues")
 public class IssuesRestTest {
 
